38193a40eb545a0644b84b9e13fecb7c1a5e4811
asensiodev <asensiodev@gmail.com>
2024-03-01 18:16:31 +0100

    Add list and list detail

---
+++ LearningSwiftUi/Views/ListDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListDetailView.swift
+//  LearningSwiftUi
+//
+//  Created by Ángel Asensio on 1/3/24.
+//
+
+import SwiftUI
+
+struct ListDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListDetailView()
+}
