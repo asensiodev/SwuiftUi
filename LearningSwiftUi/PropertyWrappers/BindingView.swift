d2f47dbc8995ade86c0f28378cb9ac87c56e9dd9
asensiodev <asensiodev@gmail.com>
2024-03-04 13:06:27 +0100

    Add state object and observed object

--- LearningSwiftUi/PropertyWrappers/BindingView.swift
+++ LearningSwiftUi/PropertyWrappers/BindingView.swift
@@ -10,17 +10,22 @@ import SwiftUI
 struct BindingView: View {
     
     @Binding var value: Int
+    @ObservedObject var user: UserData
     
     var body: some View {
         VStack {
             Text("El valor actual es \(value)").padding()
             Button("Suma 2") {
                 value += 2
+            }.padding()
+            Button("Update data") {
+                user.name = "Viriato"
+                user.age = 45
             }
         }
     }
 }
 
 #Preview {
-    BindingView(value: .constant(5))
+    BindingView(value: .constant(5), user: UserData())
 }
--- LearningSwiftUi/PropertyWrappers/StateView.swift
+++ LearningSwiftUi/PropertyWrappers/StateView.swift
@@ -7,20 +7,35 @@
 
 import SwiftUI
 
+class UserData: ObservableObject {
+    @Published var name: String = "Asensio"
+    @Published var age = 31
+}
+
 struct StateView: View {
     
     @State private var value = 0
     @State private var selection: Int?
+    @StateObject private var user = UserData()
     
     var body: some View {
         NavigationView {
             VStack {
+                HStack {
+                    Text("My name is \(user.name)").font(.title3).padding()
+                    Text("My age is \(user.age)").font(.title3).padding()
+                }
+                Button("Update data") {
+                    user.name = "Peter"
+                    user.age = 36
+                }.padding()
+                Divider()
                 Text("El valor actual es \(value)").padding()
                 Button("Suma 1") {
                     value += 1
                 }.padding()
                 NavigationLink(
-                    destination: BindingView(value: $value),
+                    destination: BindingView(value: $value, user: user),
                     tag: 1,
                     selection: $selection) {
                         Button("Ir a Binding View") {
